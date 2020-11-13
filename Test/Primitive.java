67276edd891e7081becea309f7aef9560c26bc85
Anusha-R-S <72907124+Anusha-R-S@users.noreply.github.com>
2020-11-13 18:20:06 +0530

    Add files via upload

---
+++ Test/Primitive.java
@@ -0,0 +1,41 @@
+public class Primitive
+{
+ public static void main(String[] args)
+ {
+	 System.out.println("The Program will convert string to number");
+	 
+	 String RNum="120";
+	 byte convertedRoomNum=Byte.parseByte(RNum);
+	 System.out.println("converted byte : "+convertedRoomNum);
+	 
+	 String sal="25000";
+	 short convertedSalary=Short.parseShort(sal);
+	 System.out.println("converted short: "+convertedSalary);
+	 
+	 String star="30000";
+	 int convertedStars=Integer.parseInt(star);
+	 System.out.println("converted int: "+convertedStars);
+	 
+	 String pop="25698425856";
+	 long convertedPopulation=Long.parseLong(pop);
+	 System.out.println("converted long: "+convertedPopulation);
+	 
+	 String height="5.0";
+	 float convertedHeight=Float.parseFloat(height);
+	 System.out.println("converted float: "+convertedHeight);
+	 
+	 String weight="5.9721087465876597";
+	 double convertedWeightOfEarth=Double.parseDouble(weight);
+	 System.out.println("converted double: "+convertedWeightOfEarth);
+	 
+	 String sugar="true";
+	 boolean convertedSugar=Boolean.parseBoolean(sugar);
+	 System.out.println("converted boolean: "+convertedSugar);
+	 
+	 String division="B";
+	 char convertedDivision=division.charAt(0);
+	 System.out.println("converted character: "+convertedDivision);
+
+	 
+ }
+}
---
+++ Test/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+
+
+    public static void main(String[] player)
+	{
+	     String Cricket="Virat Kohli"; 
+		 		 		
+		 System.out.println(" Cricket :"+Cricket);	 
+	}
+}
